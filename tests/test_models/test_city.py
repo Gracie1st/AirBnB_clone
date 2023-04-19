0dba304bae340e02a6e3515adb51090a0cdf3044
Gracie1st <112003488+Gracie1st@users.noreply.github.com>
2023-04-19 17:50:04 +0100

    Tasks

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,29 +1,16 @@
 #!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
+""" testing city """
+import unittest
+import pep8
 from models.city import City
-import os
 
+class City_testing(unittest.TestCase):
+    """ check BaseModel """
 
-class test_City(test_basemodel):
-    """ tests for city """
-
-    def __init__(self, *args, **kwargs):
-        """ init the test class"""
-        super().__init__(*args, **kwargs)
-        self.name = "City"
-        self.value = City
-
-    def test_state_id(self):
-        """ testing state_id type """
-        new = self.value()
-        self.assertEqual(type(new.state_id), str if
-                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
-                         type(None))
-
-    def test_name(self):
-        """ testing name type"""
-        new = self.value()
-        self.assertEqual(type(new.name), str if
-                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
-                         type(None))
+    def testpep8(self):
+        """ testing codestyle """
+        pepstylecode = pep8.StyleGuide(quiet=True)
+        path_user = 'models/city.py'
+        result = pepstylecode.check_files([path_user])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
