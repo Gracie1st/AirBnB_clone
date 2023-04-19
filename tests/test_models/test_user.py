98347a85c0ebf1957442b430560703f9cfa735e4
Gracie1st <112003488+Gracie1st@users.noreply.github.com>
2023-04-19 17:52:22 +0100

    T

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,43 +1,16 @@
 #!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
+""" testing User """
+import unittest
+import pep8
 from models.user import User
-import os
 
+class User_testing(unittest.TestCase):
+    """ check BaseModel """
 
-class test_User(test_basemodel):
-    """ test class for user model"""
-
-    def __init__(self, *args, **kwargs):
-        """ user test class init"""
-        super().__init__(*args, **kwargs)
-        self.name = "User"
-        self.value = User
-
-    def test_first_name(self):
-        """ testing user first anme attr"""
-        new = self.value()
-        self.assertEqual(type(new.first_name), str if
-                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
-                         type(None))
-
-    def test_last_name(self):
-        """ testing user last name attr"""
-        new = self.value()
-        self.assertEqual(type(new.last_name), str if
-                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
-                         type(None))
-
-    def test_email(self):
-        """ testing user email attr"""
-        new = self.value()
-        self.assertEqual(type(new.email), str if
-                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
-                         type(None))
-
-    def test_password(self):
-        """ testing user password attr"""
-        new = self.value()
-        self.assertEqual(type(new.password), str if
-                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
-                         type(None))
+    def testpep8(self):
+        """ testing codestyle """
+        pepstylecode = pep8.StyleGuide(quiet=True)
+        path_user = 'models/user.py'
+        result = pepstylecode.check_files([path_user])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
