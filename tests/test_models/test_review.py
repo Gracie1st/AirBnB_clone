42020f7c03b28e0a4bf933d018d0878969344ed7
Gracie1st <112003488+Gracie1st@users.noreply.github.com>
2023-04-19 17:51:22 +0100

    Tasks

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,36 +1,16 @@
 #!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
-from models.review import Review
-import os
+""" testing Place """
+import unittest
+import pep8
+from models.place import Place
 
+class Place_testing(unittest.TestCase):
+    """ check BaseModel """
 
-class test_review(test_basemodel):
-    """ review test class"""
-
-    def __init__(self, *args, **kwargs):
-        """ review class init"""
-        super().__init__(*args, **kwargs)
-        self.name = "Review"
-        self.value = Review
-
-    def test_place_id(self):
-        """ testing review place_id attr"""
-        new = self.value()
-        self.assertEqual(type(new.place_id), str if
-                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
-                         type(None))
-
-    def test_user_id(self):
-        """ testing review user_id attr"""
-        new = self.value()
-        self.assertEqual(type(new.user_id), str if
-                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
-                         type(None))
-
-    def test_text(self):
-        """ testing review text attr"""
-        new = self.value()
-        self.assertEqual(type(new.text), str if
-                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
-                         type(None))
+    def testpep8(self):
+        """ testing codestyle """
+        pepstylecode = pep8.StyleGuide(quiet=True)
+        path_user = 'models/place.py'
+        result = pepstylecode.check_files([path_user])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
